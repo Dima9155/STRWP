124de84e836e9358fc5bca26eb405b5e525094c3
Shiryaev Dmitriy <93621415+Dima9155@users.noreply.github.com>
2024-09-23 10:06:43 +0200

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 const Table = ({ employees, delEmployee }) => {
@@ -29,35 +29,4 @@ const Table = ({ employees, delEmployee }) => {
 };
 
 export default Table;
-=======
-import React from "react";
 
-const Table = ({ employees, delEmployee }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employee, index) => {
-          return (
-            <tr key={index}>
-              <td>{employee.name}</td>
-              <td>{employee.job}</td>
-              <td>
-                <button onClick={() => delEmployee(employee.id)}>Delete</button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
-
-export default Table;
->>>>>>> 1c9f0752c29cec85d60956ceac2b2f48fc7adb61
