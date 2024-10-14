19461e734b9fd391a60052af892e4423275f1372
Shyruaev Dmitriy <dimasiraev09@gmail.com>
2024-10-14 11:03:10 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
-import Login from './Login';
-//import Home from './Home';
-//import Dashboard from './Dashboard';
-import './App.css';
-import Form from './Form';
-import EmployeeAPI from './api/service';
-import Table from './Table';
+import "./App.css";
+import Form from "./Form";
+import EmployeeAPI from "./api/service";
+import Table from "./Table";
+import { useState } from "react";
 
 const initialEmployees = EmployeeAPI.all();
-
+console.log(initialEmployees);
 function App() {
-  const [employees, setEmployees] = useState(initialEmployees);
-  const [token, setToken] = useState(localStorage.getItem('token'));
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem('token', token);
-    } else {
-      localStorage.removeItem('token');
-    }
-  }, [token]);
+  const [employees, setEmloyees] = useState(initialEmployees);
 
   const delEmp = (id) => {
     if (EmployeeAPI.delete(id)) {
-      setEmployees(employees.filter((employee) => employee.id !== id));
+      setEmloyees(employees.filter((employee) => employee.id !== id));
     }
   };
 
   const addEmployee = (employee) => {
     const newEmployee = EmployeeAPI.add(employee);
     if (newEmployee) {
-      setEmployees([...employees, newEmployee]);
+      setEmloyees([...employees, newEmployee]);
     }
   };
 
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login setToken={setToken} />} />
-          <Route path="/Home" element={token ? (
-            <div>
-              <Form handleSubmit={addEmployee} inEmployee={{ name: '', age: '', username: '', password: '' }} />
-              <Table employees={employees} delEmployee={delEmp} />
-            </div>
-          ) : (
-            <Navigate to="/login" replace />
-          )} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Form handleSubmit={addEmployee} inEmployee={{ name: "", age: "" }} />
+      <Table employees={employees} delEmployee={delEmp} />
+    </div>
   );
 }
 
