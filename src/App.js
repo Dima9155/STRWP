3e61d458f95eb5ee78d9f4427b15a9c92dcdbc11
Shyruaev Dmitriy <dimasiraev09@gmail.com>
2024-10-21 11:22:01 +0200

    Login ang not working router(

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
-import Form from "./Form";
+import Form from "./Pages/Home/Component/Form";
 import EmployeeAPI from "./api/service";
-import Table from "./Table";
+import Table from "./Pages/Home/Component/Table";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { useState } from "react";
+import Login from "./Pages/Login/Login";
 
 const initialEmployees = EmployeeAPI.all();
 console.log(initialEmployees);
 function App() {
   const [employees, setEmloyees] = useState(initialEmployees);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const handleAuth = (username) => {
+    setIsAuthenticated(true);
+    setCurrentUser(username);
+  };
 
   const delEmp = (id) => {
     if (EmployeeAPI.delete(id)) {
@@ -23,11 +31,27 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Form handleSubmit={addEmployee} inEmployee={{ name: "", age: "" }} />
-      <Table employees={employees} delEmployee={delEmp} />
-    </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/Login" element={<Login setAuth={handleAuth} />} />
+            <Route path="/" element={
+              isAuthenticated ? (
+                <>
+                  <p>Welcome, {currentUser}!</p> {/* Приветственное сообщение */}
+                  <Form handleSubmit={addEmployee} inEmployee={{ name: "", surname: "", age: "" }} />
+                  <Table employees={employees} delEmployee={delEmp} />
+                </>
+              ) : (
+                <Navigate to="/Login" />
+              )
+            } />
+          </Routes>
+        </div>
+      </Router>
+    
   );
 }
+
 
 export default App;
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+const users = [
+    { username: "admin", password: "admin" },
+  ];
+  
+
+function Login(setAuth) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = users.find((user) => user.username === username && user.password === password);
+    if (user) {
+        setAuth(username);
+        navigate("/");
+      } else {
+        alert("Неверные данные для входа");
+      }
+    }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
